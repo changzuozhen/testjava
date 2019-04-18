dfd7c60d4eaac98e1eb5cb290a23796c3cb38cfc
changzuozhen <changzuozhen@qq.com>
2019-04-18 10:23:28 +0800

    leetcode

---
+++ src/main/java/leetcode/SudokuSolver.java
@@ -0,0 +1,83 @@
+package leetcode;
+
+/**
+ * Sudoku Solver (Hard)
+ * 数独
+ * https://leetcode.com/problems/sudoku-solver/description/
+ */
+public class SudokuSolver {
+
+    private boolean[][] rowsUsed = new boolean[9][10];
+    private boolean[][] colsUsed = new boolean[9][10];
+    private boolean[][] cubesUsed = new boolean[9][10];
+    private char[][] board;
+
+    public void solveSudoku(char[][] board) {
+        this.board = board;
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+                int num = board[i][j] - '0';
+                rowsUsed[i][num] = true;
+                colsUsed[j][num] = true;
+                cubesUsed[cubeNum(i, j)][num] = true;
+            }
+
+        for (int i = 0; i < 9; i++)
+            for (int j = 0; j < 9; j++)
+                backtracking(i, j);
+    }
+
+    private boolean backtracking(int row, int col) {
+        while (row < 9 && board[row][col] != '.') {
+            row = col == 8 ? row + 1 : row;
+            col = col == 8 ? 0 : col + 1;
+        }
+
+        if (row == 9)
+            return true;
+
+        for (int num = 1; num <= 9; num++) {
+            if (rowsUsed[row][num] || colsUsed[col][num] || cubesUsed[cubeNum(row, col)][num])
+                continue;
+            rowsUsed[row][num] = colsUsed[col][num] = cubesUsed[cubeNum(row, col)][num] = true;
+            board[row][col] = (char) (num + '0');
+            if (backtracking(row, col))
+                return true;
+            board[row][col] = '.';
+            rowsUsed[row][num] = colsUsed[col][num] = cubesUsed[cubeNum(row, col)][num] = false;
+        }
+        return false;
+    }
+
+    private int cubeNum(int i, int j) {
+        int r = i / 3;
+        int c = j / 3;
+        return r * 3 + c;
+    }
+
+    public static void main(String[] args) {
+        char[][] sudoku = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        for (int i = 0; i < sudoku.length; i++) {
+            System.out.println(sudoku[i]);
+        }
+
+        System.out.println("---------");
+        new SudokuSolver().solveSudoku(sudoku);
+        for (int i = 0; i < sudoku.length; i++) {
+            System.out.println(sudoku[i]);
+        }
+    }
+}
---
+++ src/main/java/leetcode/max_area/TestMaxArea.java
@@ -0,0 +1,55 @@
+package leetcode.max_area;
+
+/**
+ * 695. Max Area of Island (Easy)
+ * https://leetcode.com/problems/max-area-of-island/description/
+ * 查找最大的连通面积
+ */
+
+public class TestMaxArea {
+
+    private int m, n;
+    private int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0)
+            return 0;
+
+        m = grid.length;
+        n = grid[0].length;
+
+        int maxArea = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int r, int c) {
+        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0)
+            return 0;
+
+        grid[r][c] = 0;
+        int area = 1;
+        for (int[] d : direction)
+            area += dfs(grid, r + d[0], c + d[1]);
+
+        return area;
+    }
+
+    public static void main(String[] args) {
+        TestMaxArea tool = new TestMaxArea();
+        int[][] area = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+
+        System.out.println("max:" + tool.maxAreaOfIsland(area));
+    }
+
+}
